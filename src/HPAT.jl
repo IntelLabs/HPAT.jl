e84558e6243a3655dc21edb14d7a49783046a2e8
Ehsan Totoni <ehsan.totoni@intel.com>
2016-02-25 12:59:17 -0800

    Opt passes

--- src/HPAT.jl
+++ src/HPAT.jl
@@ -1,5 +1,25 @@
 module HPAT
 
-# package code goes here
+using CompilerTools.DebugMsg
+DebugMsg.init()
+
+using CompilerTools
+using CompilerTools.OptFramework
+using CompilerTools.OptFramework.OptPass
+using ParallelAccelerator
+using ParallelAccelerator.Driver
+
+
+export hpat, @acc, @noacc
+
+
+# initialize set of compiler passes HPAT runs
+const hpat = [ OptPass(captureOperators, PASS_MACRO),
+               OptPass(toCartesianArray, PASS_MACRO),
+               OptPass(toDomainIR, PASS_TYPED),
+               OptPass(toParallelIR, PASS_TYPED),
+               OptPass(toDistributedIR, PASS_TYPED),
+               OptPass(toFlatParfors, PASS_TYPED),
+               OptPass(toCGen, PASS_TYPED) ]
 
 end # module
