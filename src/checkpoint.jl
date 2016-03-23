7244b063ad522bb4f444c7f1c87a57d1029339dd
Todd A. Anderson <todd.a.anderson@intel.com>
2016-03-23 15:29:01 -0700

    Adding code to determine the intersection of the live-in's to a single loop in the checkpointed function with the set of defs in the loop. The intersection represents the data that checkpointing needs to save.

--- src/checkpoint.jl
+++ src/checkpoint.jl
@@ -23,7 +23,6 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 THE POSSIBILITY OF SUCH DAMAGE.
 =# 
 
-
 module Checkpointing
 
 import ParallelAccelerator
@@ -37,9 +36,6 @@ using CompilerTools.Helper
 
 import HPAT
 
-mk_alloc(typ, s) = Expr(:alloc, typ, s)
-mk_call(fun,args) = Expr(:call, fun, args...)
-
 # ENTRY to checkpointing
 function from_root(function_name, ast :: Expr, with_restart :: Bool)
     @assert ast.head == :lambda "Input to Checkpointing should be :lambda Expr"
@@ -49,10 +45,31 @@ function from_root(function_name, ast :: Expr, with_restart :: Bool)
     
     old_body = CompilerTools.LambdaHandling.getBody(ast)
 
-    lives = CompilerTools.LivenessAnalysis.from_expr(ast)
+    lives = ParallelAccelerator.ParallelIR.computeLiveness(ast)
     @dprintln(3,"lives = ", lives)
     loop_info = CompilerTools.Loops.compute_dom_loops(lives.cfg)
     @dprintln(3,"loop_info = ", loop_info)
+
+    loops = loop_info.loops
+
+    if length(loops) != 1
+        @dprintln(0,"Checkpointing.from_root currently only supports functions with exactly one loop.  ", function_name, " has ", length(loops), " loops.")
+        return ast
+    end
+    the_loop = loops[1]
+
+    loop_entry = the_loop.head
+    @dprintln(3,"loop_entry = ", loop_entry)
+    liveness_loop_entry = CompilerTools.LivenessAnalysis.getBasicBlockFromBlockNumber(loop_entry, lives)
+    @dprintln(3,"liveness_loop_entry = ", liveness_loop_entry)
+    bb_loop_members = map(x -> CompilerTools.LivenessAnalysis.getBasicBlockFromBlockNumber(x, lives), the_loop.members)
+    @dprintln(3,"bb_loop_members = ", bb_loop_members)
+    loop_live_in = liveness_loop_entry.live_in
+    @dprintln(3,"loop_live_in = ", loop_live_in)
+    loop_def = reduce((x,y) -> union(x,y), Set{SymGen}(), map(x -> x.def, bb_loop_members))
+    @dprintln(3,"loop_def = ", loop_def)
+    live_in_and_def = intersect(loop_live_in, loop_def)
+    @dprintln(3,"live_in_and_def = ", live_in_and_def)
 
 #    body = TypedExpr(CompilerTools.LambdaHandling.getReturnType(state.linfo), :body, from_toplevel_body(CompilerTools.LambdaHandling.getBody(state.linfo), state)...)
 #    new_ast = CompilerTools.LambdaHandling.LambdaVarInfoToLambdaExpr(state.linfo, body)
