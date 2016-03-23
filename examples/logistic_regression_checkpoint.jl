92745bd7b8158d95d068cd32f48e3760cb37e8ac
Ehsan Totoni <ehsan.totoni@intel.com>
2016-03-23 15:28:10 -0700

    checkpoit version of logistic_regression in separate file

--- examples/logistic_regression_checkpoint.jl
+++ examples/logistic_regression_checkpoint.jl
@@ -27,7 +27,11 @@ using HPAT
 using MPI
 using DocOpt
 
-@acc hpat function logistic_regression(iterations, file_name)
+#CompilerTools.OptFramework.set_debug_level(3)
+#ParallelAccelerator.ParallelIR.set_debug_level(3)
+#ParallelAccelerator.CGen.set_debug_level(3)
+
+@acc hpat_checkpoint function logistic_regression(iterations, file_name)
     points = DataSource(Matrix{Float32},HDF5,"/points", file_name)
     responses = DataSource(Vector{Float32},HDF5,"/responses", file_name)
     D = size(points,1) # number of features
