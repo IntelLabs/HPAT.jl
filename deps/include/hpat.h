e7422182e2adf617f9ebe86610fcbb95216e1ef9
Todd A. Anderson <todd.a.anderson@intel.com>
2016-03-30 12:19:47 -0700

    Turn on checkpoint debug messages by default.
    Keep the estimated checkpoint time in C code and have Julia access it via a call to hpat_get_checkpoint_time.
    
    Print out some checkpoint frequency and timing information at the end of a checkpoint.

--- deps/include/hpat.h
+++ deps/include/hpat.h
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <ctime>
 
-//#define CHECKPOINT_DEBUG
+#define CHECKPOINT_DEBUG
 
 class file_j2c_array_io : public j2c_array_io {
 protected:
@@ -25,9 +25,6 @@ public:
 
     virtual void write(void *arr, uint64_t arr_length, unsigned int elem_size, bool immutable) {
         write_in(arr, arr_length, elem_size, immutable);
-        //checkpoint_file->write((char*)&arr_length, sizeof(arr_length));
-        //checkpoint_file->write((char*)&elem_size,  sizeof(elem_size));
-        //checkpoint_file->write((char*)arr, arr_length * elem_size);
     }
 
     virtual void read(void **arr, uint64_t *length) {
@@ -49,6 +46,11 @@ int32_t g_checkpoint_handle = 0;
 std::fstream checkpoint_file;
 int64_t g_unique;
 int32_t g_checkpoint_start_time;
+double  g_checkpoint_time = 1.0 / 60.0; // Estimated 1 min checkpoint time.  1/60 of hour.
+
+double __hpat_get_checkpoint_time(int64_t unique_checkpoint_location) {
+    return g_checkpoint_time;
+}
 
 int32_t __hpat_start_checkpoint(int64_t unique_checkpoint_location) {
     g_checkpoint_start_time = std::time(nullptr);
@@ -58,7 +60,7 @@ int32_t __hpat_start_checkpoint(int64_t unique_checkpoint_location) {
     MPI_Comm_rank(MPI_COMM_WORLD,&__hpat_node_id);
     if (__hpat_node_id == 0) {
 #ifdef CHECKPOINT_DEBUG
-        std::cout << "__hpat_start_checkpoint location = " << unique_checkpoint_location << std::endl;
+        std::cout << "__hpat_start_checkpoint location = " << unique_checkpoint_location << " checkpoint# = " << g_checkpoint_handle + 1 << " start_time = " << g_checkpoint_start_time << std::endl;
 #endif
         checkpoint_file.open("hpat_checkpoint_in_progress", std::ios::out | std::ios::binary);
         if (checkpoint_file.fail()) {
@@ -164,7 +166,16 @@ int32_t __hpat_end_checkpoint(int32_t checkpoint_handle) {
         rename("hpat_checkpoint_in_progress", ss.str().c_str()); 
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    return std::time(nullptr) - g_checkpoint_start_time;
+    int32_t cur_time = std::time(nullptr);
+    int32_t checkpoint_time = cur_time - g_checkpoint_start_time;
+    if (checkpoint_time < 1) {
+        checkpoint_time = 1;
+    }
+    g_checkpoint_time = checkpoint_time / 3600.0;
+#ifdef CHECKPOINT_DEBUG
+    std::cout << "__hpat_end_checkpoint cur_time(s) = " << cur_time << " checkpoint_length(s) = " << checkpoint_time << " new_checkpoint_est(h) = " << g_checkpoint_time << std::endl;
+#endif
+    return 0;
 }
 
 int32_t __hpat_finish_checkpoint_region(int64_t unique_checkpoint_location) {
@@ -180,7 +191,7 @@ int32_t __hpat_finish_checkpoint_region(int64_t unique_checkpoint_location) {
 
 
 
-// Restore functions below.
+// Restore functions are implemented below.
 
 
 int32_t __hpat_restore_checkpoint_start(int64_t unique_checkpoint_location) {
