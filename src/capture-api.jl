4643ec3c6e16d3ac88815b35085ff3b5337afde3
Ehsan Totoni <ehsan.totoni@intel.com>
2016-05-22 11:05:22 -0700

    translate aggregate WIP

--- src/capture-api.jl
+++ src/capture-api.jl
@@ -1,6 +1,7 @@
 module CaptureAPI
 
 using CompilerTools
+using CompilerTools.AstWalker
 import ..API
 import HPAT
 
@@ -50,11 +51,52 @@ function process_assignment(node, state, lhs::Symbol, rhs::Expr)
         state[lhs] = [new_key;rest_cols1;rest_cols2]
         return :( ($new_key_arr,$(rest_cols3_arrs...)) = join([$key1_arr;$(rest_cols1_arrs...)], [$key2_arr;$(rest_cols2_arrs...)]) )
         
+   elseif rhs.head==:call && rhs.args[1]==:aggregate
+        t1 = rhs.args[2]
+        c1 = rhs.args[3].value
+        c1_arr = getColName(t1, c1)
+        c1_out_arr = getColName(lhs, c1)
+        out_e = []
+        out_aggs = []
+        out_arrs = [c1_out_arr]
+        out_cols = [c1]
+        for col_expr in rhs.args[4:end]
+            @assert col_expr.head==:kw "expected assignment for new aggregate column"
+            # output column name
+            out_col = col_expr.args[1].value
+            out_col_arr = getColName(lhs, out_col)
+            push!(out_cols, out_col)
+            push!(out_arrs, out_col_arr)
+            @assert col_expr.args[2].head==:call "expected aggregation function"
+            # aggregation function
+            func = col_expr.args[2].args[1]
+            # aggregation expression
+            e = col_expr.args[2].args[2]
+            # replace column name with actual array in expression
+            AstWalk(e, replace_col_with_array,  (t1, state[t1]))
+            out_e_arr = symbol("_$(lhs)_$(out_col)_e")
+            push!(out_aggs, :(($out_e_arr, $func)))
+            push!(out_e,:($out_e_arr=$e))
+        end
+        out_call = :($(out_arrs...) = aggregate($c1_arr,[$(out_aggs...)]))
+        push!(out_e, out_call)
+        state[lhs] = out_cols
+        return quote $(out_e...) end
    end
    CompilerTools.AstWalker.ASTWALK_RECURSE
 end
 
 function process_assignment(node, state, lhs::ANY, rhs::ANY)
+end
+
+function replace_col_with_array(node::QuoteNode, table::Tuple{Symbol,Vector{Symbol}}, top_level_number, is_top_level, read)
+    if node.value in table[2]
+        return getColName(table[1], node.value)
+    end
+    return CompilerTools.AstWalker.ASTWALK_RECURSE
+end
+function replace_col_with_array(node::ANY, table::Tuple{Symbol,Vector{Symbol}}, top_level_number, is_top_level, read)
+    return CompilerTools.AstWalker.ASTWALK_RECURSE
 end
 
 function translate_data_source(lhs, state, arr_var_expr, source_typ, other_args)
@@ -79,7 +121,7 @@ function translate_data_table(lhs, state, arr_var_expr, source_typ, other_args)
         push!(col_names, col_name)
         col_type = column.args[2]
         col_lhs = getColName(lhs,col_name)
-        col_source = translate_data_source(col_lhs, :(Vector{$(col_type)}), source_typ, other_args)
+        col_source = translate_data_source(col_lhs, state, :(Vector{$(col_type)}), source_typ, other_args)
         push!(out, col_source)
     end
     state[lhs] = col_names
