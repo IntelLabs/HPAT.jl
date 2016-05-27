0a52cb77eeca80ad5b7fde3949c2536214e054e2
Wajihulhassan <wajih.lums@gmail.com>
2016-05-27 07:09:03 -0700

    adding mpi test

---
+++ test/mpi-test.jl
@@ -0,0 +1,13 @@
+import MPI
+
+function main()
+    MPI.Init()
+
+    comm = MPI.COMM_WORLD
+    println("Hello world, I am $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
+    MPI.Barrier(comm)
+
+    MPI.Finalize()
+end
+
+main()
