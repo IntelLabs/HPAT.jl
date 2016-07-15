8b860d9f229c4f9efc6f46f7d3abcc3e7457f650
Wajihulhassan <wajih.lums@gmail.com>
2016-07-14 23:09:15 -0700

    making testing uniform

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Base.Test
 
-run(`mpirun -np 2 julia --depwarn=no mpi-test.jl`)
+run(`mpirun -np 2 julia --depwarn=no mpi_test.jl`)
 run(`mpirun -np 2 julia --depwarn=no sink_test.jl`)
 run(`julia --depwarn=no q26_test.jl`)
 #run(`mpirun -np 4 julia --depwarn=no 2d_test.jl`)
