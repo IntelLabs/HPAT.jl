a9c75dd8050a18606062d92237eb02f7c509b750
Wajihulhassan <wajih.lums@gmail.com>
2016-07-20 18:35:12 -0700

    using dataframes for copying dataset to hdf5

--- examples/queries_devel/q26/create_data_test_q26.jl
+++ examples/queries_devel/q26/create_data_test_q26.jl
@@ -1,24 +1,13 @@
 using HDF5
-
+using DataFrames
 # Arguments:
 # ARGS[1] = option
 # ARGS[2] = table store_sales path e.g."/home/whassan/tmp/csv/store_sales_sanitized.csv"
 # ARGS[3] = table item path e.g. "/home/whassan/tmp/csv/item_sanitized.csv"
-option = parse(Int,ARGS[1])
-if  option == 1 && length(ARGS) >= 3
-    table1_path=ARGS[2]
-    table2_path=ARGS[3]
-end
 
-customer = []
-sale = []
-item = []
-class = []
-category = []
+#num_rows = 2000000000000
 
-num_rows = 2000000000000
-
-if option == 0
+function example_dataset(customer,sale,item,class,category)
     #=
     customer    iterm
     1       1
@@ -28,63 +17,71 @@ if option == 0
     2       3
     3       1
     =#
-    customer = [1,1,2,1,2,3]
-    sale = [1,2,1,3,3,1]
+    customer_tmp = [1,1,2,1,2,3]
+    append!(customer,customer_tmp)
+    sale_tmp = [1,2,1,3,3,1]
+    append!(sale,sale_tmp)
 
     #= item class category
     1   3   1
     2   1   2
     3   2   1
     =#
-    item = [1,2,3]
-    class = [3,1,2]
-    category = [1,2,1]
+    item_tmp = [1,2,3]
+    append!(item,item_tmp)
+    class_tmp = [3,1,2]
+    append!(class, class_tmp)
+    category_tmp = [1,2,1]
+    append!(category, category_tmp)
+    
 end
 
-if option == 1
+function generate_dataset(customer,sale,item,class,category,table1_path,table2_path)
     store_sales_file = open(table1_path)
     item_file = open(table2_path)
     counter = 0
-    for line in readlines(item_file)
-        line = chomp(line)
-        line_splited = split(line,",")
-        push!(item,parse(Int64,line_splited[1]))
-        push!(class,parse(Int64,line_splited[2]))
-        push!(category,parse(Int64,line_splited[3]))
-        counter = counter + 1
-        if counter >= num_rows
-            break
-        end
-    end
-    counter = 0
-    for line in readlines(store_sales_file)
-        line = chomp(line)
-        line_splited = split(line,",")
-        push!(sale,parse(Int64,line_splited[1]))
-        push!(customer,parse(Int64,line_splited[2]))
-        counter = counter + 1
-        if counter >= num_rows
-            break
-        end
+    item_df = readtable(item_file)
+    append!(item,item_df[1])
+    append!(class,item_df[2])
+    append!(category,item_df[3])
 
-    end
-    # Explicit conversion of types. Idk how to declare them
-    customer = convert(Array{Int64,1},customer)
-    sale = convert(Array{Int64,1},sale)
-    item = convert(Array{Int64,1},item)
-    class = convert(Array{Int64,1},class)
-    category = convert(Array{Int64,1},category)
+    ss_df = readtable(store_sales_file)
+    append!(sale,ss_df[1])
+    append!(customer,ss_df[2])
 end
 
-file_name = "test_q26.hdf5"
+function main()
 
-if isfile(file_name)
-    rm(file_name)
+    option = parse(Int,ARGS[1])
+    if  option == 1 && length(ARGS) >= 3
+        table1_path=ARGS[2]
+        table2_path=ARGS[3]
+    end
+
+    customer = Int64[]
+    sale = Int64[]
+    item = Int64[]
+    class = Int64[]
+    category = Int64[]
+
+    if option == 0
+        example_dataset(customer,sale,item,class,category)
+    end
+
+    if option == 1
+        generate_dataset(customer,sale,item,class,category,table1_path,table2_path)
+    end
+    println(":D Done reading into arrays whole dataset")
+    file_name = "test_q26.hdf5"
+
+    if isfile(file_name)
+        rm(file_name)
+    end
+    h5write(file_name ,"/ss_customer_sk",customer)
+    h5write(file_name, "/ss_item_sk",sale)
+
+    h5write(file_name, "/i_item_sk", item)
+    h5write(file_name, "/i_class_id", class)
+    h5write(file_name, "/i_category", category)
 end
-
-h5write(file_name ,"/ss_customer_sk",customer)
-h5write(file_name, "/ss_item_sk",sale)
-
-h5write(file_name, "/i_item_sk", item)
-h5write(file_name, "/i_class_id", class)
-h5write(file_name, "/i_category", category)
+main()
