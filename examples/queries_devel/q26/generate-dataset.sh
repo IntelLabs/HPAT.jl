b27a99cf2ebd56957d9d6bbc59390d7faf9c4569
Wajihulhassan <wajih.lums@gmail.com>
2016-07-25 16:00:24 -0700

    updated dataset generation to make to further decrease dataset before sed

--- examples/queries_devel/q26/generate-dataset.sh
+++ examples/queries_devel/q26/generate-dataset.sh
@@ -6,7 +6,7 @@ BIG_DATA_BENCHMARK_PATH=/home/whassan/Big-Data-Benchmark-for-Big-Bench/
 DATASET_PATH=/home.old/whassan/tmp/csv/
 NUM_MAP_TASKS=2
 
-for dataset_factor in 500 ; do
+for dataset_factor in 600 ; do
     if [ -f ${DATASET_PATH}/store_sales_${dataset_factor}f.dat ]; then
         rm ${DATASET_PATH}/store_sales_${dataset_factor}f.dat
     fi
@@ -14,16 +14,15 @@ for dataset_factor in 500 ; do
         rm ${DATASET_PATH}/item_${dataset_factor}f.dat
     fi
 
-    ${BIG_DATA_BENCHMARK_PATH}/bin/bigBench dataGen -U -m NUM_MAP_TASKS -f $dataset_factor
+    ${BIG_DATA_BENCHMARK_PATH}/bin/bigBench dataGen -U -m ${NUM_MAP_TASKS} -f $dataset_factor
 
     for i in $(seq ${NUM_MAP_TASKS}); do
         hadoop fs -cat /user/whassan/benchmarks/bigbench/data/store_sales/store_sales_${i}.dat > ${DATASET_PATH}/store_sales_${i}_${dataset_factor}f.dat
 
         hadoop fs -cat /user/whassan/benchmarks/bigbench/data/item/item_${i}.dat > ${DATASET_PATH}/item_${i}_${dataset_factor}f.dat
+        cat ${DATASET_PATH}/store_sales_${i}_${dataset_factor}f.dat | cut -d'|' -f3,4 > ${DATASET_PATH}/store_sales_sanitized_${i}_${dataset_factor}f.csv &&  sed -i 's/|/,/g' ${DATASET_PATH}/store_sales_sanitized_${i}_${dataset_factor}f.csv
 
-        sed -i 's/|/,/g' ${DATASET_PATH}/store_sales_${i}_${dataset_factor}f.dat && cat ${DATASET_PATH}/store_sales_${i}_${dataset_factor}f.dat | cut -d, -f3,4 > ${DATASET_PATH}/store_sales_sanitized_${i}_${dataset_factor}f.csv
-
-        sed -i 's/|/,/g' ${DATASET_PATH}/item_${i}_${dataset_factor}f.dat && cat ${DATASET_PATH}/item_${i}_${dataset_factor}f.dat | cut -d, -f1,10,13 |  sed 's/[.!@#$%^&*()-]//g' | sed -e 's,_,,g' | tr abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 012345678901234567890123456789  | cut -d ' ' -f1  > ${DATASET_PATH}/item_sanitized_${i}_${dataset_factor}f.csv
+         cat ${DATASET_PATH}/item_${i}_${dataset_factor}f.dat | cut -d'|' -f1,10,13 |  sed 's/[.!@#$%^&*()-]//g' | sed -e 's,_,,g' | tr abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 012345678901234567890123456789  | cut -d ' ' -f1  > ${DATASET_PATH}/item_sanitized_${i}_${dataset_factor}f.csv && sed -i 's/|/,/g' ${DATASET_PATH}/item_sanitized_${i}_${dataset_factor}f.csv
     done
     cat ${DATASET_PATH}/store_sales_sanitized_[0-9]_${dataset_factor}f.csv > ${DATASET_PATH}/store_sales_sanitized_${dataset_factor}f.csv
     cat ${DATASET_PATH}/item_sanitized_[0-9]_${dataset_factor}f.csv > ${DATASET_PATH}/item_sanitized_${dataset_factor}f.csv
