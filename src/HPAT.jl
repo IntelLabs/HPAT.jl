b6a7676601580b7c7d189004cd3b88a824453751
Wajihulhassan <wajih.lums@gmail.com>
2016-07-28 01:16:23 -0700

    Pass over HPAT, incorporating julia style guide; fixing indentation; capitalize global variable; sorting imports; removing unnecessary imports; using shorter syntax for conditional

--- src/HPAT.jl
+++ src/HPAT.jl
@@ -32,14 +32,14 @@ using CompilerTools.DebugMsg
 DebugMsg.init()
 
 using CompilerTools
+using CompilerTools.AstWalker
 using CompilerTools.OptFramework
 using CompilerTools.OptFramework.OptPass
 using ParallelAccelerator
 using ParallelAccelerator.Driver
 using ParallelAccelerator.CGen
-using CompilerTools.AstWalker
+
 import MPI
-using Base.pushmeta!
 
 export hpat, hpat_checkpoint, hpat_debug, @acc, @noacc
 
@@ -59,14 +59,14 @@ OptFramework.setSaveOriginalFunction(false)
 force_parallel = true
 
 function setForceParallel(v::Bool)
-  global force_parallel=v
+    global force_parallel=v
 end
 
 # constant block size for 2D partitioning
 BLOCK_SIZE = 128
 
 function setBlockSize(v::Int)
-  global BLOCK_SIZE=v
+    global BLOCK_SIZE=v
 end
 
 # smaller value means higher precedence
@@ -84,13 +84,13 @@ include("cgen-hpat-pattern-match.jl")
 
 # overwrite print so only rank 0 prints
 #=function Base.print(xs...)
-    rank = MPI.Comm_rank(MPI.COMM_WORLD)
-    if rank==0 Base.print(STDOUT, xs) end
+rank = MPI.Comm_rank(MPI.COMM_WORLD)
+if rank==0 Base.print(STDOUT, xs) end
 end
 
 function Base.println(xs...)
-    rank = MPI.Comm_rank(MPI.COMM_WORLD)
-    if rank==0 Base.println(STDOUT, xs) end
+rank = MPI.Comm_rank(MPI.COMM_WORLD)
+if rank==0 Base.println(STDOUT, xs) end
 end=#
 
 
@@ -98,62 +98,62 @@ end=#
 ParallelAccelerator.CGen.setExternalPatternMatchCall(CGenPatternMatch.pattern_match_call)
 ParallelAccelerator.CGen.setExternalPatternMatchAssignment(CGenPatternMatch.from_assignment_match_dist)
 
-HPAT_path = joinpath(dirname(@__FILE__), "..")
+HPAT_PATH = joinpath(dirname(@__FILE__), "..")
 
-HPAT_includes = string("#include <ctime>\n#include <stdlib.h>\n#include \"", HPAT_path, "/deps/include/hpat.h\"\n")
-ParallelAccelerator.CGen.addCgenUserOptions(ParallelAccelerator.CGen.CgenUserOptions(HPAT_includes))
+HPAT_INCLUDES = string("#include <ctime>\n#include <stdlib.h>\n#include \"", HPAT_PATH, "/deps/include/hpat.h\"\n")
+ParallelAccelerator.CGen.addCgenUserOptions(ParallelAccelerator.CGen.CgenUserOptions(HPAT_INCLUDES))
 
 function addHpatInclude(stmts)
-  ParallelAccelerator.CGen.addCgenUserOptions(ParallelAccelerator.CGen.CgenUserOptions(stmts))
+    ParallelAccelerator.CGen.addCgenUserOptions(ParallelAccelerator.CGen.CgenUserOptions(stmts))
 end
 
 function ns_to_sec(x)
-  x / 1000000000.0
+    x / 1000000000.0
 end
 
 function runDistributedPass(func :: GlobalRef, ast, signature :: Tuple)
-  dir_start = time_ns()
-  code = DistributedPass.from_root(string(func.name), ast)
-  dir_time = time_ns() - dir_start
-  @dprintln(2, "Distributed code = ", code)
-  @dprintln(1, "accelerate: DistributedPass conversion time = ", ns_to_sec(dir_time))
-  return code
+    dir_start = time_ns()
+    code = DistributedPass.from_root(string(func.name), ast)
+    dir_time = time_ns() - dir_start
+    @dprintln(2, "Distributed code = ", code)
+    @dprintln(1, "accelerate: DistributedPass conversion time = ", ns_to_sec(dir_time))
+    return code
 end
 
 function runDataTablePass(func :: GlobalRef, ast, signature :: Tuple)
-  dir_start = time_ns()
-  code = DataTablePass.from_root(string(func.name), ast)
-  dir_time = time_ns() - dir_start
-  @dprintln(2, "DataTable code = ", code)
-  @dprintln(1, "accelerate: DataTablePass conversion time = ", ns_to_sec(dir_time))
-  return code
+    dir_start = time_ns()
+    code = DataTablePass.from_root(string(func.name), ast)
+    dir_time = time_ns() - dir_start
+    @dprintln(2, "DataTable code = ", code)
+    @dprintln(1, "accelerate: DataTablePass conversion time = ", ns_to_sec(dir_time))
+    return code
 end
 
 function runDomainPass(func :: GlobalRef, ast, signature :: Tuple)
-  dir_start = time_ns()
-  code = DomainPass.from_root(string(func.name), ast)
-  dir_time = time_ns() - dir_start
-  @dprintln(2, "Domain code = ", code)
-  @dprintln(1, "accelerate: DomainPass conversion time = ", ns_to_sec(dir_time))
-  return code
+    dir_start = time_ns()
+    code = DomainPass.from_root(string(func.name), ast)
+    dir_time = time_ns() - dir_start
+    @dprintln(2, "Domain code = ", code)
+    @dprintln(1, "accelerate: DomainPass conversion time = ", ns_to_sec(dir_time))
+    return code
 end
 
 function addCheckpointing(func :: GlobalRef, ast, signature :: Tuple)
-  dir_start = time_ns()
-  code = Checkpointing.from_root(string(func.name), ast, false)
-  dir_time = time_ns() - dir_start
-  @dprintln(2, "Added checkpointing = ", code)
-  @dprintln(1, "accelerate: addCheckpointing conversion time = ", ns_to_sec(dir_time))
-  return code
+    dir_start = time_ns()
+    code = Checkpointing.from_root(string(func.name), ast, false)
+    dir_time = time_ns() - dir_start
+    @dprintln(2, "Added checkpointing = ", code)
+    @dprintln(1, "accelerate: addCheckpointing conversion time = ", ns_to_sec(dir_time))
+    return code
 end
 
 function addCheckpointingRestart(func :: GlobalRef, ast, signature :: Tuple)
-  dir_start = time_ns()
-  code = Checkpointing.from_root(string(func.name), ast, true)
-  dir_time = time_ns() - dir_start
-  @dprintln(2, "Added checkpointing and restart = ", code)
-  @dprintln(1, "accelerate: addCheckpointingRestart conversion time = ", ns_to_sec(dir_time))
-  return code
+    dir_start = time_ns()
+    code = Checkpointing.from_root(string(func.name), ast, true)
+    dir_time = time_ns() - dir_start
+    @dprintln(2, "Added checkpointing and restart = ", code)
+    @dprintln(1, "accelerate: addCheckpointingRestart conversion time = ", ns_to_sec(dir_time))
+    return code
 end
 
 type MacroState
@@ -169,67 +169,68 @@ type MacroState
 end
 
 """
-A macro pass that translates extensions such as DataSource()
+    A macro pass that translates extensions such as DataSource()
 """
 function captureHPAT(func, ast, sig)
-  macro_state = MacroState()
-  AstWalk(ast, CaptureAPI.process_node, macro_state)
-  pushmeta!(ast,:hpat_tables, macro_state.tableCols, macro_state.tableTypes)
-  pushmeta!(ast,:hpat_partitioning, macro_state.array_partitioning)
-  #println(ast)
-  return ast
+    macro_state = MacroState()
+    AstWalk(ast, CaptureAPI.process_node, macro_state)
+    Base.pushmeta!(ast,:hpat_tables, macro_state.tableCols, macro_state.tableTypes)
+    Base.pushmeta!(ast,:hpat_partitioning, macro_state.array_partitioning)
+    return ast
 end
 
-""" Adds a duplicate of the function to be checkpointed with "_restart" appended to the name.
+""" 
+    Adds a duplicate of the function to be checkpointed with "_restart" appended to the name.
 """
 function createCheckpointFunc(func, ast, sig)
-  @dprintln(1, "createCheckpointFunc func = ", func)
-  new_func = deepcopy(ast)
-  new_func.args[1].args[1] = symbol(string(new_func.args[1].args[1],"_restart"))
-  @dprintln(1, "createCheckpointFunc new_func = ", new_func.args[1].args[1])
-  return CompilerTools.OptFramework.MoreWork(ast, CompilerTools.OptFramework.WorkItem[CompilerTools.OptFramework.WorkItem(hpat_checkpoint_internal, hpat_checkpoint_internal, Any[], new_func)])
+    @dprintln(1, "createCheckpointFunc func = ", func)
+    new_func = deepcopy(ast)
+    new_func.args[1].args[1] = symbol(string(new_func.args[1].args[1],"_restart"))
+    @dprintln(1, "createCheckpointFunc new_func = ", new_func.args[1].args[1])
+    return CompilerTools.OptFramework.MoreWork(ast, CompilerTools.OptFramework.WorkItem[CompilerTools.OptFramework.WorkItem(hpat_checkpoint_internal, hpat_checkpoint_internal, Any[], new_func)])
 end
 
 const hpat_debug = [ OptPass(captureHPAT, PASS_MACRO) ]
 
 # initialize set of compiler passes HPAT runs
-const hpat = [ OptPass(captureHPAT, PASS_MACRO),
-               OptPass(captureOperators, PASS_MACRO),
-               OptPass(toCartesianArray, PASS_MACRO),
-               OptPass(runDomainPass, PASS_TYPED),
-               OptPass(toDomainIR, PASS_TYPED),
-               OptPass(runDataTablePass, PASS_TYPED),
-               OptPass(toParallelIR, PASS_TYPED),
-               OptPass(runDistributedPass, PASS_TYPED),
-               OptPass(toFlatParfors, PASS_TYPED),
-               OptPass(toCGen, PASS_TYPED) ]
+const hpat =
+    [ OptPass(captureHPAT, PASS_MACRO),
+      OptPass(captureOperators, PASS_MACRO),
+      OptPass(toCartesianArray, PASS_MACRO),
+      OptPass(runDomainPass, PASS_TYPED),
+      OptPass(toDomainIR, PASS_TYPED),
+      OptPass(runDataTablePass, PASS_TYPED),
+      OptPass(toParallelIR, PASS_TYPED),
+      OptPass(runDistributedPass, PASS_TYPED),
+      OptPass(toFlatParfors, PASS_TYPED),
+      OptPass(toCGen, PASS_TYPED) ]
 
 const hpat_checkpoint =
-             [ OptPass(createCheckpointFunc, PASS_MACRO),
-               OptPass(captureHPAT, PASS_MACRO),
-               OptPass(captureOperators, PASS_MACRO),
-               OptPass(toCartesianArray, PASS_MACRO),
-               OptPass(runDomainPass, PASS_TYPED),
-               OptPass(addCheckpointing, PASS_TYPED),
-               OptPass(toDomainIR, PASS_TYPED),
-               OptPass(runDataTablePass, PASS_TYPED),
-               OptPass(toParallelIR, PASS_TYPED),
-               OptPass(runDistributedPass, PASS_TYPED),
-               OptPass(toFlatParfors, PASS_TYPED),
-               OptPass(toCGen, PASS_TYPED) ]
+    [ OptPass(createCheckpointFunc, PASS_MACRO),
+      OptPass(captureHPAT, PASS_MACRO),
+      OptPass(captureOperators, PASS_MACRO),
+      OptPass(toCartesianArray, PASS_MACRO),
+      OptPass(runDomainPass, PASS_TYPED),
+      OptPass(addCheckpointing, PASS_TYPED),
+      OptPass(toDomainIR, PASS_TYPED),
+      OptPass(runDataTablePass, PASS_TYPED),
+      OptPass(toParallelIR, PASS_TYPED),
+      OptPass(runDistributedPass, PASS_TYPED),
+      OptPass(toFlatParfors, PASS_TYPED),
+      OptPass(toCGen, PASS_TYPED) ]
 
 const hpat_checkpoint_internal =
-             [ OptPass(captureHPAT, PASS_MACRO),
-               OptPass(captureOperators, PASS_MACRO),
-               OptPass(toCartesianArray, PASS_MACRO),
-               OptPass(runDomainPass, PASS_TYPED),
-               OptPass(addCheckpointingRestart, PASS_TYPED),
-               OptPass(toDomainIR, PASS_TYPED),
-               OptPass(runDataTablePass, PASS_TYPED),
-               OptPass(toParallelIR, PASS_TYPED),
-               OptPass(runDistributedPass, PASS_TYPED),
-               OptPass(toFlatParfors, PASS_TYPED),
-               OptPass(toCGen, PASS_TYPED) ]
+    [ OptPass(captureHPAT, PASS_MACRO),
+      OptPass(captureOperators, PASS_MACRO),
+      OptPass(toCartesianArray, PASS_MACRO),
+      OptPass(runDomainPass, PASS_TYPED),
+      OptPass(addCheckpointingRestart, PASS_TYPED),
+      OptPass(toDomainIR, PASS_TYPED),
+      OptPass(runDataTablePass, PASS_TYPED),
+      OptPass(toParallelIR, PASS_TYPED),
+      OptPass(runDistributedPass, PASS_TYPED),
+      OptPass(toFlatParfors, PASS_TYPED),
+      OptPass(toCGen, PASS_TYPED) ]
 
 append!(ParallelAccelerator.DomainIR.funcIgnoreList, map(x->GlobalRef(HPAT.API,x),[DomainPass.generatedFuncs; API.operators]))
 append!(ParallelAccelerator.DomainIR.exprHeadIgnoreList, DomainPass.generatedExprHeads)
@@ -237,27 +238,24 @@ ParallelAccelerator.DomainIR.setExternalCallback(DomainPass.AstWalkCallback)
 ParallelAccelerator.DomainIR.setExternalLiveCB(DomainPass.live_cb)
 ParallelAccelerator.DomainIR.setExternalAliasCB(DomainPass.alias_cb)
 
-
-HPAT_default_datapath = "input_data/"
+HPAT_DEFAULT_DATAPATH = "input_data/"
 
 if haskey(ENV, "HPAT_DEFAULT_DATA")
-    global HPAT_default_datapath = ENV["HPAT_DEFAULT_DATA"]
+    global HPAT_DEFAULT_DATAPATH = ENV["HPAT_DEFAULT_DATA"]
 elseif haskey(ENV, "SCRATCH")
-    global HPAT_default_datapath = ENV["SCRATCH"]*"/input_data/"
+    global HPAT_DEFAULT_DATAPATH = ENV["SCRATCH"]*"/input_data/"
 else
-    global HPAT_default_datapath = joinpath(dirname(@__FILE__), "..")*"/input_data/"
+    global HPAT_DEFAULT_DATAPATH = joinpath(dirname(@__FILE__), "..")*"/input_data/"
 end
 
-if MPI.Comm_rank(MPI.COMM_WORLD)==0 && !isdir(HPAT_default_datapath)
-    mkdir(HPAT_default_datapath)
-end
+MPI.Comm_rank(MPI.COMM_WORLD)==0 && !isdir(HPAT_DEFAULT_DATAPATH) &&  mkdir(HPAT_DEFAULT_DATAPATH)
 
 function getDefaultDataPath()
-    return HPAT_default_datapath
+    return HPAT_DEFAULT_DATAPATH
 end
 
 function setDefaultDataPath(path::AbstractString)
-    HPAT_default_datapath = path
+    HPAT_DEFAULT_DATAPATH = path
 end
 
 function HPAT_finalize()
@@ -268,27 +266,27 @@ end
 atexit(HPAT_finalize)
 
 function restart(func, args...)
-  @dprintln(1, "HPAT restart func = ", func, " type = ", typeof(func), " args = ", args...)
-  arg_type_tuple_expr = Expr(:tuple)
-  arg_type_tuple_expr.args = map(x -> typeof(x), [args...])
-  arg_type_tuple = eval(arg_type_tuple_expr)
-  @dprintln(2, "arg_type_tuple = ", arg_type_tuple)
-  gr = GlobalRef(Base.function_module(func,arg_type_tuple), symbol(func))
-  @dprintln(2, "GlobalRef = ", gr)
-  if haskey(CompilerTools.OptFramework.gOptFrameworkDict, gr)
-    res = CompilerTools.OptFramework.gOptFrameworkDict[gr]
-    @dprintln(2, "CompilerTools.OptFramework internally mapped ", gr, " to ", res)
-    assert(typeof(res) == GlobalRef)
-    new_gr = GlobalRef(res.mod, symbol(string(res.name,"_restart")))
-    @dprintln(2, "Preparing to call the restart version of the function with name = ", new_gr)
-    eval(new_gr)(args...)
-  else
-    throw(string("HPAT restart function could not find function to restart."))
-  end
+    @dprintln(1, "HPAT restart func = ", func, " type = ", typeof(func), " args = ", args...)
+    arg_type_tuple_expr = Expr(:tuple)
+    arg_type_tuple_expr.args = map(x -> typeof(x), [args...])
+    arg_type_tuple = eval(arg_type_tuple_expr)
+    @dprintln(2, "arg_type_tuple = ", arg_type_tuple)
+    gr = GlobalRef(Base.function_module(func,arg_type_tuple), symbol(func))
+    @dprintln(2, "GlobalRef = ", gr)
+    if haskey(CompilerTools.OptFramework.gOptFrameworkDict, gr)
+        res = CompilerTools.OptFramework.gOptFrameworkDict[gr]
+        @dprintln(2, "CompilerTools.OptFramework internally mapped ", gr, " to ", res)
+        assert(typeof(res) == GlobalRef)
+        new_gr = GlobalRef(res.mod, symbol(string(res.name,"_restart")))
+        @dprintln(2, "Preparing to call the restart version of the function with name = ", new_gr)
+        eval(new_gr)(args...)
+    else
+        throw(string("HPAT restart function could not find function to restart."))
+    end
 end
 
 @noinline function __hpat_dist_broadcast(variable, size)
-   convert(Int32, 1)
+    convert(Int32, 1)
 end
 
 end # module
