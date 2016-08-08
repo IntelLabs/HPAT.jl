523e661f7fd996d9ecfca94d46ba3dedb30186c0
Wajihulhassan <wajih.lums@gmail.com>
2016-08-08 15:59:44 -0700

    scripts to generate and run q05. we should really add strings in hdf5 to make this simpler

---
+++ examples/queries_devel/q05/generate-dataset.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+#set -e
+set -x
+
+BIG_DATA_BENCHMARK_PATH=/home/whassan/Big-Data-Benchmark-for-Big-Bench/
+DATASET_PATH=/home.old/whassan/tmp/csv/q05/
+mkdir -p ${DATASET_PATH}
+NUM_MAP_TASKS=2
+
+for dataset_factor in 200 250; do
+    if [ -f ${DATASET_PATH}/web_clickstreams_${dataset_factor}f.dat ]; then
+        rm ${DATASET_PATH}/web_clickstreams_${dataset_factor}f.dat
+    fi
+    if [ -f ${DATASET_PATH}/item_${dataset_factor}f.dat ]; then
+        rm ${DATASET_PATH}/item_${dataset_factor}f.dat
+    fi
+    if [ -f ${DATASET_PATH}/customer_${dataset_factor}f.dat ]; then
+        rm ${DATASET_PATH}/customer_${dataset_factor}f.dat
+    fi
+    if [ -f ${DATASET_PATH}/customer_demographics_${dataset_factor}f.dat ]; then
+        rm ${DATASET_PATH}/customer_demographics_${dataset_factor}f.dat
+    fi
+
+    ${BIG_DATA_BENCHMARK_PATH}/bin/bigBench dataGen -U -m ${NUM_MAP_TASKS} -f $dataset_factor
+
+    for i in $(seq ${NUM_MAP_TASKS}); do
+	hadoop fs -cat /home.old/whassan/user/whassan/benchmarks/bigbench/data/web_clickstreams/web_clickstreams_${i}.dat > ${DATASET_PATH}/web_clickstreams_${i}_${dataset_factor}f.dat
+	hadoop dfsadmin -safemode leave
+	hadoop fs -rmr /home.old/whassan/user/whassan/benchmarks/bigbench/data/web_clickstreams/web_clickstreams_${i}.dat
+	hadoop fs -cat /home.old/whassan/user/whassan/benchmarks/bigbench/data/item/item_${i}.dat > ${DATASET_PATH}/item_${i}_${dataset_factor}f.dat
+	hadoop fs -rmr /home.old/whassan/user/whassan/benchmarks/bigbench/data/item/item_${i}.dat
+	hadoop fs -cat /home.old/whassan/user/whassan/benchmarks/bigbench/data/customer/customer_${i}.dat > ${DATASET_PATH}/customer_${i}_${dataset_factor}f.dat
+	hadoop fs -rmr /home.old/whassan/user/whassan/benchmarks/bigbench/data/customer/customer_${i}.dat
+	# For some reasons second data is not generated
+	#if [ $i -lt 2 ]; then
+            hadoop fs -cat /home.old/whassan/user/whassan/benchmarks/bigbench/data/customer_demographics/customer_demographics_${i}.dat > ${DATASET_PATH}/customer_demographics_${i}_${dataset_factor}f.dat
+	    hadoop fs -rmr /home.old/whassan/user/whassan/benchmarks/bigbench/data/customer_demographics/customer_demographics_${i}.dat
+	#fi
+	cat ${DATASET_PATH}/web_clickstreams_${i}_${dataset_factor}f.dat  | cut -d'|' -f4,6 > ${DATASET_PATH}/web_clickstreams_sanitized_${i}_${dataset_factor}f.dat
+	rm ${DATASET_PATH}/web_clickstreams_${i}_${dataset_factor}f.dat
+	sed -i 's/|/,/g' ${DATASET_PATH}/web_clickstreams_sanitized_${i}_${dataset_factor}f.dat
+
+	cat ${DATASET_PATH}/item_${i}_${dataset_factor}f.dat  | cut -d'|' -f1,12,13 |  sed 's/[.!@#$%^&*()-]//g' | sed -e 's,_,,g' | tr abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 012345678901234567890123456789  | cut -d ' ' -f1 > ${DATASET_PATH}/item_sanitized_${i}_${dataset_factor}f.dat
+	rm ${DATASET_PATH}/item_${i}_${dataset_factor}f.dat
+	sed -i 's/|/,/g' ${DATASET_PATH}/item_sanitized_${i}_${dataset_factor}f.dat
+
+
+	cat ${DATASET_PATH}/customer_${i}_${dataset_factor}f.dat | cut -d'|' -f1,3 >  ${DATASET_PATH}/customer_sanitized_${i}_${dataset_factor}f.dat
+	rm ${DATASET_PATH}/customer_${i}_${dataset_factor}f.dat
+	sed -i 's/|/,/g' ${DATASET_PATH}/customer_sanitized_${i}_${dataset_factor}f.dat
+
+	#if [ $i -lt 2 ]; then
+	    cat ${DATASET_PATH}/customer_demographics_${i}_${dataset_factor}f.dat  | cut -d'|' -f1,2,4 | sed 's/[.!@#$%^&*()-]//g' | sed -e 's,_,,g' | tr abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 012345678901234567890123456789012345678  | cut -d ' ' -f1 > ${DATASET_PATH}/customer_demographics_sanitized_${i}_${dataset_factor}f.dat
+	    rm ${DATASET_PATH}/customer_demographics_${i}_${dataset_factor}f.dat
+	    sed -i 's/|/,/g' ${DATASET_PATH}/customer_demographics_sanitized_${i}_${dataset_factor}f.dat
+	#fi
+     done
+
+    cat ${DATASET_PATH}/web_clickstreams_sanitized_[0-9]_${dataset_factor}f.dat > ${DATASET_PATH}/web_clickstreams_sanitized_${dataset_factor}f.csv
+    cat ${DATASET_PATH}/item_sanitized_[0-9]_${dataset_factor}f.dat > ${DATASET_PATH}/item_sanitized_${dataset_factor}f.csv
+    cat ${DATASET_PATH}/customer_sanitized_[0-9]_${dataset_factor}f.dat > ${DATASET_PATH}/customer_sanitized_${dataset_factor}f.csv
+    cat ${DATASET_PATH}/customer_demographics_sanitized_[0-9]_${dataset_factor}f.dat > ${DATASET_PATH}/customer_demographics_sanitized_${dataset_factor}f.csv
+    hadoop dfsadmin -safemode leave
+    hadoop fs -rmr /home.old/whassan/user/whassan/benchmarks/bigbench/data/
+
+    rm ${DATASET_PATH}/web_clickstreams_sanitized_[0-9]_${dataset_factor}f.dat
+    rm ${DATASET_PATH}/item_sanitized_[0-9]_${dataset_factor}f.dat
+    rm ${DATASET_PATH}/customer_sanitized_[0-9]_${dataset_factor}f.dat
+    rm ${DATASET_PATH}/customer_demographics_sanitized_[0-9]_${dataset_factor}f.dat
+
+done
