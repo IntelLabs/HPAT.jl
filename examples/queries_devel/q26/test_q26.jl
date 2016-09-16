ecc6d405643775f54912ba2c49fd432d8ba15b28
Ehsan Totoni <ehsan.totoni@intel.com>
2016-09-15 19:07:50 -0700

    q26 minor bugfix

--- examples/queries_devel/q26/test_q26.jl
+++ examples/queries_devel/q26/test_q26.jl
@@ -35,7 +35,7 @@ using HPAT
 
     customer_i_class = customer_i_class[:ss_item_count>item_count]
 
-    points = transpose(Base.typed_hcat(Int64,customer_i_class[:ss_customer_sk], customer_i_class[:id1], customer_i_class[:id2],
+    points = transpose(Base.typed_hcat(Int64,customer_i_class[:ss_item_count], customer_i_class[:id1], customer_i_class[:id2],
     customer_i_class[:id3], customer_i_class[:id4], customer_i_class[:id5], customer_i_class[:id6],
     customer_i_class[:id7], customer_i_class[:id8], customer_i_class[:id9], customer_i_class[:id10],
     customer_i_class[:id11], customer_i_class[:id12], customer_i_class[:id13], customer_i_class[:id14], customer_i_class[:id15]))
