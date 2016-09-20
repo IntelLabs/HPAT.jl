943e7be387d430ac163e389bc24afc858f2e7fd0
Ehsan Totoni <ehsan.totoni@intel.com>
2016-09-20 12:47:53 -0700

    update q25 syntax

--- examples/queries_devel/q25/q25.jl
+++ examples/queries_devel/q25/q25.jl
@@ -1,5 +1,7 @@
 using HPAT
 
+#HPAT.DomainPass.set_debug_level(3)
+
 @acc hpat function q25(d_date, file_name)
     store_sales = DataSource(DataTable{:ss_customer_sk=Int64, :ss_ticket_number=Int64, :ss_sold_date_sk=Int64, :ss_net_paid=Float64}, HDF5, file_name)
     web_sales = DataSource(DataTable{:ws_bill_customer_sk=Int64, :ws_order_number=Int64, :ws_sold_date_sk=Int64, :ws_net_paid=Float64}, HDF5, file_name)
@@ -7,22 +9,23 @@ using HPAT
     store_sales = store_sales[:ss_sold_date_sk > d_date]
     web_sales   = web_sales[:ws_sold_date_sk > d_date]
 
-    store_agg = aggregate(store_sales, :cid = :ss_customer_sk, 
-                                           :frequency = count(union(:ss_ticket_number)),
-                                           :most_recent_date = max(:ss_sold_date_sk),
+    store_agg = aggregate(store_sales, :cid = :ss_customer_sk,
+                                           :frequency = length(unique(:ss_ticket_number)),
+                                           :most_recent_date = maximum(:ss_sold_date_sk),
                                            :amount = sum(:ss_net_paid))
     web_agg = aggregate(web_sales,  :cid = :ws_bill_customer_sk,
-                                           :frequency = count(union(:ws_order_number)),
-                                           :most_recent_date = max(:ws_sold_date_sk),
+                                           :frequency = length(unique(:ws_order_number)),
+                                           :most_recent_date = maximum(:ws_sold_date_sk),
                                            :amount = sum(:ws_net_paid))
     web_store_agg = [store_agg; web_agg]
 
     result = aggregate(web_store_agg, :cid,
-                                      :recency = (37621 - max(:most_recent_date) < 60 ? 1.0 : 0.0),
+                                      :recency = maximum(:most_recent_date),
                                       :frequency = sum(:frequency),
                                       :totalspend = sum(:amount))
+#    result[:recency] = (37621 - result[:recency] < 60 ? 1.0 : 0.0);
     return result[:cid], result[:recency], result[:frequency], result[:totalspend]
 
 end
 
-println(q25("33000", "test_q25.hdf5"))
+println(q25(33000, "test_q25.hdf5"))
