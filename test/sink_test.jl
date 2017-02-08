ee52b6444fba46a30329be20860723191551eb15
Ehsan Totoni <ehsan.totoni@intel.com>
2017-02-07 19:01:17 -0500

    multidimension source sink test

--- test/sink_test.jl
+++ test/sink_test.jl
@@ -7,10 +7,17 @@ using HPAT
     DataSink(y,HDF5,"/y", file3)
 end
 
+@acc hpat function sink2(file3)
+    y = randn(3,10)
+    DataSink(y,HDF5,"/y2", file3)
+end
+
+
 using MPI
 
 function main()
     SinkTest.sink("y.hdf5")
+    SinkTest.sink2("y.hdf5")
     if MPI.Comm_rank(MPI.COMM_WORLD)==0 rm("y.hdf5") end
 end
 
--- test/source_test.jl
+++ test/source_test.jl
@@ -9,6 +9,10 @@ using HPAT
     return sum(A)
 end
 
+@acc hpat function stest2(file_name)
+    A = DataSource(Matrix{Float64},HDF5,"/A2", file_name)
+    return sum(A)
+end
 
 using HDF5
 using MPI
@@ -18,9 +22,11 @@ function main()
 
     if MPI.Comm_rank(MPI.COMM_WORLD)==0 
         h5write("A.hdf5", "/A", [1.0,2.0,3.0,4.0])
+        h5write("A.hdf5", "/A2", [1.0 2.0 3.0; 4.0 5.0 6.0])
     end
 
     @test_approx_eq SourceTest.stest("A.hdf5") 10.0
+    @test_approx_eq SourceTest.stest2("A.hdf5") 21.0
     if MPI.Comm_rank(MPI.COMM_WORLD)==0 rm("A.hdf5") end
 end
 
