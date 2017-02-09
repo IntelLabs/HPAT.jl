842d6ba228f0128f1867341d4aee8ee5a1c73b43
Ehsan Totoni <ehsan.totoni@intel.com>
2017-02-09 08:13:27 -0800

    txt sink tests

--- test/sink_test.jl
+++ test/sink_test.jl
@@ -12,12 +12,24 @@ end
     DataSink(y,HDF5,"/y2", file3)
 end
 
+@acc hpat function sink_txt(file3)
+    y = randn(10)
+    DataSink(y, TXT, file3)
+end
+
+@acc hpat function sink_txt2(file3)
+    y = randn(3,10)
+    DataSink(y, TXT, file3)
+end
+
 
 using MPI
 
 function main()
     SinkTest.sink("y.hdf5")
     SinkTest.sink2("y.hdf5")
+    SinkTest.sink_txt("y.txt")
+    SinkTest.sink_txt2("y2.txt")
     if MPI.Comm_rank(MPI.COMM_WORLD)==0 rm("y.hdf5") end
 end
 
